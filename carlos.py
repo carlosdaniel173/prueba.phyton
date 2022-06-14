fe08dbef900b39f65e73306056ebf563edc1d06c
carlosdaniel173 <107499720+carlosdaniel173@users.noreply.github.com>
2022-06-14 12:06:02 -0500

    Add files via upload
    ya.

---
+++ carlos.py
@@ -0,0 +1,26 @@
+while True:
+    print("0.menu")
+    print("comienza el programa")
+    print("2 imprime el listado")
+    print("3 finaliza el programa")
+
+
+    opcion=0
+    int(input("que opcion desea ejecautar : "))
+
+if opcion == 2:
+    print("programa iniciado")
+
+if opcion == 1:
+    print("enpieza tu listado")
+    print("mandarina,azucar,arroz")
+
+elif opcion == 3:
+    print("has finalizado el programa")
+
+
+
+else:
+
+    print("error")
+
